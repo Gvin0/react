ab05a06960b8fe392af87e05b8376439b01032ec
Levan Gviniashvili <levanigvino@gmail.com>
2020-02-20 23:42:05 +0400

    remove useless state

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -6,10 +6,6 @@ import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSumm
 import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
-    state = {
-        ingredients: {},
-        totalPrice: 0
-    }
 
     checkoutCancelledHandler = () => {
         this.props.history.goBack();
